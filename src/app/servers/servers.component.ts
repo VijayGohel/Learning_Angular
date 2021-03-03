cafb9b29a2eccb735f4e2d152601e617a5331645
vijay gohel <gohil5898@gmail.com>
2021-03-03 15:03:55 +0530

    styling

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,7 +4,8 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-servers',
   //template: '<h1>Hello</h1>',
   templateUrl: './servers.component.html' ,
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
+  styles: ['div { color :green;}']
 })
 export class ServersComponent implements OnInit {
 
