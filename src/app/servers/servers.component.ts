e414b95598ccadf7681f66f52fab5e43ac634bb2
vijay gohel <gohil5898@gmail.com>
2021-03-03 15:17:36 +0530

    selectors

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,10 +2,12 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
+  //selector: '.app-servers',
+ // selector:'[app-servers]',
   //template: '<h1>Hello</h1>',
   templateUrl: './servers.component.html' ,
-  styleUrls: ['./servers.component.css'],
-  styles: ['div { color :green;}']
+   styleUrls: ['./servers.component.css'],
+ // styles: ['div { color :green;}']
 })
 export class ServersComponent implements OnInit {
 
