5887a579451f06e25dbbd6c0fe6a71b57c63f3d2
vijay gohel <gohil5898@gmail.com>
2021-03-03 14:49:33 +0530

    creating component using CLI

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  //template: '<h1>Hello</h1>',
+  templateUrl: './servers.component.html' ,
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
